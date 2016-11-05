6435788a1434b402fa51579ed0c3577e2bec7b73
Jesus Caño <jcanovel@gmail.com>
2016-11-05 23:44:14 +0100

    Add code

--- ch06e01/vector_type.js
+++ ch06e01/vector_type.js
@@ -1,4 +1,19 @@
-// Your code here.
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Object.defineProperty(Vector.prototype, "length", {
+    get: function() { return Math.sqrt(this.x * this.x + this.y * this.y); }
+});
+
+Vector.prototype.plus = function(v) {
+    return new Vector(this.x + v.x, this.y + v.y);
+};
+
+Vector.prototype.minus = function(v) {
+    return new Vector(this.x - v.x, this.y - v.y);
+};
 
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
