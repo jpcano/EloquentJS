280fcdadcf431f5d0bedaac0aed0385e614e1878
Jesus Caño <jcanovel@gmail.com>
2016-11-15 00:24:35 +0100

    Functional version

--- ch07e02/predators.js
+++ ch07e02/predators.js
@@ -1,27 +1,60 @@
-// Your code here
-function Tiger() {}
+function SmartPlantEater() {
+    this.energy = 20;
+}
+
+SmartPlantEater.prototype.act = function(view) {
+    var width = view.world.grid.width;
+    var height = view.world.grid.height;
+    var size = (width * height)
+    
+    var space = view.find(" ");
+    if (this.energy > 50 && space) {
+		return {type: "reproduce", direction: space};
+    }
+    var plant = view.find("*");
+    if (plant)
+		return {type: "eat", direction: plant};
+    if (space)
+		return {type: "move", direction: space};
+};
+
+function Tiger() {
+    this.energy = 99;
+}
+
+Tiger.prototype.act = function(view) {
+    var space = view.find(" ");
+    if (this.energy > 100 && space) {
+		return {type: "reproduce", direction: space};
+    }
+    var planteater = view.find("O");
+    if (planteater)
+		return {type: "eat", direction: planteater};
+    if (space)
+		return {type: "move", direction: space};
+};
 
 animateWorld(new LifelikeWorld(
-    ["####################################################",
-     "#                 ####         ****              ###",
-     "#   *  @  ##                 ########       OO    ##",
-     "#   *    ##        O O                 ****       *#",
-     "#       ##*                        ##########     *#",
-     "#      ##***  *         ****                     **#",
-     "#* **  #  *  ***      #########                  **#",
-     "#* **  #      *               #   *              **#",
-     "#     ##              #   O   #  ***          ######",
-     "#*            @       #       #   *        O  #    #",
-     "#*                    #  ######                 ** #",
-     "###          ****          ***                  ** #",
-     "#       O                        @         O       #",
-     "#   *     ##  ##  ##  ##               ###      *  #",
-     "#   **         #              *       #####  O     #",
-     "##  **  O   O  #  #    ***  ***        ###      ** #",
-     "###               #   *****                    ****#",
-     "####################################################"],
-    {"#": Wall,
-     "@": Tiger,
-     "O": SmartPlantEater, // from previous exercise
-     "*": Plant}
+  ["####################################################",
+   "#                 ####         ****              ###",
+   "#   *  @  ##                 ########       OO    ##",
+   "#   *    ##        O O                 ****       *#",
+   "#       ##*                        ##########     *#",
+   "#      ##***  *         ****                     **#",
+   "#* **  #  *  ***      #########                  **#",
+   "#* **  #      *               #   *              **#",
+   "#     ##              #   O   #  ***          ######",
+   "#*            @       #       #   *        O  #    #",
+   "#*                    #  ######                 ** #",
+   "###          ****          ***                  ** #",
+   "#       O                        @         O       #",
+   "#   *     ##  ##  ##  ##               ###      *  #",
+   "#   **         #              *       #####  O     #",
+   "##  **  O   O  #  #    ***  ***        ###      ** #",
+   "###               #   *****                    ****#",
+   "####################################################"],
+  {"#": Wall,
+   "@": Tiger,
+   "O": SmartPlantEater, // from previous exercise
+   "*": Plant}
 ));
