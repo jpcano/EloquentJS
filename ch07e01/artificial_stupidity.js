b9e0b7a10eb7beaf19ac8b5d315c1347c4ad757b
Jesus Caño <jcanovel@gmail.com>
2016-11-14 23:59:36 +0100

    Improvements

--- ch07e01/artificial_stupidity.js
+++ ch07e01/artificial_stupidity.js
@@ -21,22 +21,24 @@ SmartPlantEater.prototype.act = function(view) {
     var width = view.world.grid.width;
     var height = view.world.grid.height;
     var size = (width * height)
-    
+
+    // Rest after reproduce
     if (this.rest > 0) {
-  	this.rest--;
-	return null;
+    	this.rest--;
+      	return {type: "move", direction: view.vector};
     }
     var space = view.find(" ");
+    // Reproduce if there are enough plants
     if (this.energy > 60 && space && view.countAll("*") > size / 2) {
-	this.rest = 16;
-	return {type: "reproduce", direction: space};
+		this.rest = 32;
+		return {type: "reproduce", direction: space};
     }
     var plant = view.find("*");
-    if (plant)
-	return {type: "eat", direction: plant};
+    // Do no eat if there are few plants
+    if (plant && view.countAll("*") > size / 10)
+		return {type: "eat", direction: plant};
     if (space)
-	return {type: "move", direction: space};
-    console.log(view.countAll("*"));
+		return {type: "move", direction: space};
 };
 
 animateWorld(new LifelikeWorld(
