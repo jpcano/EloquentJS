8e43cb32592857f668b53d63fca2725b7dfa2532
Jesus Caño <jcanovel@gmail.com>
2017-01-16 12:42:53 +0100

    Initial commit

---
+++ ch17e02/all.js
@@ -0,0 +1,44 @@
+function all(promises) {
+    var pending = promises.length;
+    var result = [];
+    return new Promise(function (success, fail) {
+        promises.forEach(function (promise, i) {
+            promise.then(function (val) {
+                result[i] = val;
+                pending -= 1;
+                if (pending == 0)
+                    success(result);
+            },
+                function (error) {
+                    fail(error);
+                });
+        });
+        if (promises.length == 0)
+            success(result);
+    });
+}
+
+// Test code.
+all([]).then(function (array) {
+    console.log("This should be []:", array);
+});
+function soon(val) {
+    return new Promise(function (success) {
+        setTimeout(function () { success(val); },
+            Math.random() * 500);
+    });
+}
+all([soon(1), soon(2), soon(3)]).then(function (array) {
+    console.log("This should be [1, 2, 3]:", array);
+});
+function fail() {
+    return new Promise(function (success, fail) {
+        fail(new Error("boom"));
+    });
+}
+all([soon(1), fail(), soon(3)]).then(function (array) {
+    console.log("We should not get here");
+}, function (error) {
+    if (error.message != "boom")
+        console.log("Unexpected failure:", error);
+});
